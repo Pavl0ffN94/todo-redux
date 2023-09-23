fe7e2d6ae909f2d0f2f1da1bbf2cdc3002b362af
Pavl0ffN <pavl0ffn94@gmail.com>
2023-09-23 16:32:36 +0400

    first commit

---
+++ src/components/Filters.jsx
@@ -0,0 +1,22 @@
+import { Link, useParams } from 'react-router-dom';
+
+const Filters = () => {
+  const { filter: activeFilter = 'all' } = useParams();
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', gap: '1rem' }}>
+      <Link to='/all' style={{ color: activeFilter === 'all' ? 'red' : 'black' }}>
+        all
+      </Link>
+      <Link to='/active' style={{ color: activeFilter === 'active' ? 'red' : 'black' }}>
+        active
+      </Link>
+      <Link
+        to='/comleted'
+        style={{ color: activeFilter === 'completed' ? 'red' : 'black' }}>
+        completed
+      </Link>
+    </div>
+  );
+};
+
+export { Filters };
